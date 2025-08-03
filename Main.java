3277dfa86e9e6592bb0a5813b24e59df39b54feb
MadhuLC <madhusree.bellamkonda@gmail.com>
2025-08-03 11:25:08 -0400

    updated goodbye world

--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello World Welcome to Madhu's Java World!");
+        System.out.println("Goodbye, world!");
     }
 }
